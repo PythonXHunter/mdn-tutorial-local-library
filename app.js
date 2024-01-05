ae09cb163458adc96c17b188222340ad2acad756
PythonXHunter <aryan.computerscience@gmail.com>
2024-01-05 14:46:17 +0530

    resolve error

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
 
-main().catch((err) => console.log(errr));
+main().catch((err) => console.log(err));
 async function main(){
   await mongoose.connect(process.env.mongoDB, {
     useNewUrlParser: true,
